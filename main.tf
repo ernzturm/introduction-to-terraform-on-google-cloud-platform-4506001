990bc605bd08f7f3b306ccb3961e2070aadd5e76
ernzturm <44079185+ernzturm@users.noreply.github.com>
2024-10-10 22:51:58 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,17 +16,18 @@ data "google_compute_image" "ubuntu" {
   family      = "ubuntu-2204-lts"
 }
 
-resource "google_compute_instance" "blog" {
-  name         = "blog"
+resource "google_compute_instance" "web" {
+  name         = "web"
   machine_type = "e2-micro"
 
+  
   boot_disk {
     initialize_params {
       image = data.google_compute_image.ubuntu.self_link
     }
   }
   network_interface {
-   subnetwork = "app"
+   subnetwork = "default"
    access_config {
       # Leave empty for dynamic public IP
     }
