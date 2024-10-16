8eae595b41c513e8fc07c26d7975451e6a037629
ernzturm <44079185+ernzturm@users.noreply.github.com>
2024-10-16 00:00:12 -0600

    blog

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ resource "google_compute_network" "app" {
   auto_create_subnetworks = false
 }
 
+
 resource "google_compute_subnetwork" "app" {
   name          = "app"
   ip_cidr_range = "10.2.0.0/16"
